932b8b4cad070ea0a4e73616ff91b72f8c0353ab
Azat2k <dzhanov.azat@gmail.com>
2024-05-18 22:34:28 -0500

    added Supplier model

---
+++ Models/Supplier.cs
@@ -0,0 +1,13 @@
+﻿namespace RepairShopV2.Models
+{
+    public class Supplier
+    {
+        int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public virtual ICollection<SparePart> SpareParts { get; } = new List<SparePart>();
+    }
+}
