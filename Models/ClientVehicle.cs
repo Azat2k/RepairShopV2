256d8c3ef98af56c9d32c5289473dffdb5961c1c
Azat2k <dzhanov.azat@gmail.com>
2024-06-01 23:33:49 -0500

    Added  ClientVehicle Model

---
+++ Models/ClientVehicle.cs
@@ -0,0 +1,14 @@
+﻿namespace RepairShopV2.Models
+{
+    public class ClientVehicle
+    {
+        public int Id { get; set; }
+        public int MakeId { get; set; }
+        public int ModelId { get; set; }
+        public int Year { get; set; }
+        public string VIN { get; set; } = string.Empty;
+        public string LicensePlate { get; set; } = string.Empty;
+        public int ClientId { get; set; }
+        public virtual Client Client { get; set; }
+    }
+}
