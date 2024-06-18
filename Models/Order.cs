c86cf39ef41156219647eb3ca66e0fb3732e2327
Azat2k <dzhanov.azat@gmail.com>
2024-06-17 23:55:28 -0500

    Added Order model

---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace RepairShopV2.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int ClientId { get; set; }
+        public virtual Client? Client { get; set; }
+        public int ClientVehicleId { get; set; }
+        public virtual ClientVehicle? ClientVehicle { get; set; }
+        //public virtual ICollection<OrderItem> OrderItems { get; } = new List<OrderItem>();
+    }
+}
