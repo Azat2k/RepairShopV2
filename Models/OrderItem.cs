6b5be094fa191625cd7ff67f0575968adcb60b48
Azat2k <dzhanov.azat@gmail.com>
2024-06-21 23:05:36 -0500

    Added OrderItem model

---
+++ Models/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿namespace RepairShopV2.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public virtual Order Order { get; set; }
+        public int? SparePartId { get; set; }
+        public virtual SparePart SparePart { get; set; }
+        public int? ServiceId { get; set; }
+        public virtual Service Service { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
