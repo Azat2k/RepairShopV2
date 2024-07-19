5c3886968f0d160f7c83c975d56436f1d15727a9
Azat2k <dzhanov.azat@gmail.com>
2024-07-18 22:01:03 -0600

    Added IdentityDataInitializer.

---
+++ Identity/IdentityDataInitializer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RepairShopV2.Identity
+{
+    public class IdentityDataInitializer
+    {
+        public static async Task SeedData(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            string[] roleNames = { "Admin", "Client", "Employee" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            // Create a default admin user
+            var adminUser = new ApplicationUser
+            {
+                UserName = "admin@admin.com",
+                Email = "admin@admin.com"
+            };
+
+            string adminPassword = "Admin@123";
+
+            var user = await userManager.FindByEmailAsync("admin@admin.com");
+
+            if (user == null)
+            {
+                var createAdminUser = await userManager.CreateAsync(adminUser, adminPassword);
+                if (createAdminUser.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+        }
+    }
+}
