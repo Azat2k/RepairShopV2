4b970ebcabe479253894aa70ff67b3663b4b9ecd
Azat2k <dzhanov.azat@gmail.com>
2024-07-22 23:03:54 -0500

    Added Login and Register ViewModels

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RepairShopV2.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
